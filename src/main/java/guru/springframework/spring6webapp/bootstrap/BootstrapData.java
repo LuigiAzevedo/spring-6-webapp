3dba48fa8fb61b350b9a8efbc1a609e7ffd906a9
LuigiAzevedo <luigiazevedo@gmail.com>
2023-10-09 20:53:18 -0300

    feat: publisher entity

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,43 +13,54 @@ import org.springframework.stereotype.Component;
 public class BootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(
+            AuthorRepository authorRepository,
+            BookRepository bookRepository,
+            PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Author eric = new Author();
-        eric.setFirstName("Eric");
-        eric.setLastName("Evans");
-
-        Author rod = new Author();
-        rod.setFirstName("Rod");
-        rod.setLastName("Johnson");
-
         Book ddd = new Book();
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
+        bookRepository.save(ddd);
 
         Book noEJB = new Book();
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("789456");
+        bookRepository.save(noEJB);
 
-        Author ericSaved = authorRepository.save(eric);
-        Author rodSaved = authorRepository.save(rod);
-        Book dddSaved = bookRepository.save(ddd);
-        Book noEJBSaved = bookRepository.save(noEJB);
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+        eric.getBooks().add(ddd);
+        authorRepository.save(eric);
 
-        ericSaved.getBooks().add(dddSaved);
-        rodSaved.getBooks().add(noEJBSaved);
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
+        rod.getBooks().add(noEJB);
+        authorRepository.save(rod);
 
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
+        Publisher pub1 = new Publisher();
+        pub1.setPublisherName("My Publisher 1");
+        pub1.setAddress("123 Street");
+        publisherRepository.save(pub1);
+
+        Publisher pub2 = new Publisher();
+        pub2.setPublisherName("My Publisher 2");
+        pub2.setAddress("456 Street");
+        publisherRepository.save(pub2);
 
         System.out.println("In Bootstrap");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
